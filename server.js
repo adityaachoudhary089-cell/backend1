218d8aae9afca2aed613583abf3202c728a5d73c
adityaachoudhary089-cell <adityaachoudhary089@gmail.com>
2025-09-25 20:52:00 +0530

    all

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use('/api/auth/signup', authLimiter);
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.ALLOWED_ORIGINS?.split(',') 
-    : ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000', 'http://127.0.0.1:5173','https://backend1-rexi.onrender.com','https://testing-drontend.vercel.app'],
+    : ['https://backend1-rexi.onrender.com','https://testing-drontend.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
